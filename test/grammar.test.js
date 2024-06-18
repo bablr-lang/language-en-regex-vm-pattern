dc433034fc5d2067444caff79956b07c36b641e3
Conrad Buck <conartist6@gmail.com>
2024-06-18 08:22:41 -0600

    *Token

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -9,16 +9,16 @@ let enhancers = undefined;
 
 // enhancers = debugEnhancers;
 
-const json = (...args) =>
+const regex = (...args) =>
   printPrettyCSTML(buildTag(language, 'Pattern', undefined, enhancers)(...args));
 
 describe('@bablr/language-regex-vm-pattern', () => {
   it('`/2/`', () => {
-    expect(json`/2/`).toEqual(dedent`\
+    expect(regex`/2/`).toEqual(dedent`\
       <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
       <>
         <Pattern>
-          open: <*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
           alternatives[]:
           <Alternative>
             elements[]:
@@ -26,16 +26,126 @@ describe('@bablr/language-regex-vm-pattern', () => {
               '2'
             </>
           </>
-          separators: null
-          close: <*Punctuator '/' balancer />
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
           flags:
-          <Flags>
-            global: null
-            ignoreCase: null
-            multiline: null
-            dotAll: null
-            unicode: null
-            sticky: null
+          <Flags !global !ignoreCase !multiline !dotAll !unicode !sticky>
+          </>
+        </>
+      </>\n`);
+  });
+
+  it('`//`', () => {
+    expect(regex`//`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
+      <>
+        <Pattern>
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          alternatives[]:
+          <Alternative>
+            elements[]: null
+          </>
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
+          flags:
+          <Flags !global !ignoreCase !multiline !dotAll !unicode !sticky>
+          </>
+        </>
+      </>\n`);
+  });
+
+  it('`//i`', () => {
+    expect(regex`//i`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
+      <>
+        <Pattern>
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          alternatives[]:
+          <Alternative>
+            elements[]: null
+          </>
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
+          flags:
+          <Flags !global ignoreCase !multiline !dotAll !unicode !sticky>
+            tokens[]:
+            <*Keyword>
+              'i'
+            </>
+          </>
+        </>
+      </>\n`);
+  });
+
+  it('`//mi`', () => {
+    expect(regex`//mi`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
+      <>
+        <Pattern>
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          alternatives[]:
+          <Alternative>
+            elements[]: null
+          </>
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
+          flags:
+          <Flags !global ignoreCase multiline !dotAll !unicode !sticky>
+            tokens[]:
+            <*Keyword>
+              'm'
+            </>
+            tokens[]:
+            <*Keyword>
+              'i'
+            </>
+          </>
+        </>
+      </>\n`);
+  });
+
+  it('`/\\W/`', () => {
+    expect(regex`/\W/`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
+      <>
+        <Pattern>
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          alternatives[]:
+          <Alternative>
+            elements[]:
+            <+WordCharacterSet negate>
+              escapeToken: <~*Punctuator '${'\\\\'}' />
+              value: <~*Keyword 'W' />
+            </>
+          </>
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
+          flags:
+          <Flags !global !ignoreCase !multiline !dotAll !unicode !sticky>
+          </>
+        </>
+      </>\n`);
+  });
+
+  it('`//<gap>`', () => {
+    const flags = buildTag(language, 'Flags', undefined, enhancers)`i`;
+    expect(regex`//${flags}`).toEqual(dedent`\
+      <!0:cstml bablr-language='https://bablr.org/languages/core/bablr-regex-pattern'>
+      <>
+        <Pattern>
+          openToken: <~*Punctuator '/' balanced='/' balancedSpan='Pattern' />
+          alternatives[]:
+          <Alternative>
+            elements[]: null
+          </>
+          separators[]: null
+          closeToken: <~*Punctuator '/' balancer />
+          flags:
+          <Flags !global ignoreCase !multiline !dotAll !unicode !sticky>
+            tokens[]:
+            <*Keyword>
+              'i'
+            </>
           </>
         </>
       </>\n`);
