24d2bcddc1beb86f47db259bea733aaaef968ae0
Conrad Buck <conartist6@gmail.com>
2026-02-26 16:14:36 -0700

    Always emit root fragment

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -26,298 +26,343 @@ describe('@bablr/language-en-regex-vm-pattern', () => {
 
     it('`//`', () => {
       expect(print(regex`//`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$: <$Alternative />
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$: <$Alternative />
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
+          </>
         </>\n`);
     });
 
     it('`/2/`', () => {
       expect(print(regex`/2/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$: <*Character '2' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$: <*Character '2' />
+            </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/21/`', () => {
       expect(print(regex`/21/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$: <*Character '2' />
-            elements[]+$: <*Character '1' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$: <*Character '2' />
+              elements[]+$: <*Character '1' />
+            </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/1|2/`', () => {
       expect(print(regex`/1|2/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$: <*Character '1' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$: <*Character '1' />
+            </>
+            #separatorTokens: <* '|' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$: <*Character '2' />
+            </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          #separatorTokens: <* '|' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$: <*Character '2' />
-          </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/2+/`', () => {
       expect(print(regex`/2+/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$: <*Character '2' />
-            ^^^
-            <$Quantifier { min: 1, max: +Infinity }>
-              element+$: <//>
-              sigilToken*: <*Keyword '+' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$: <*Character '2' />
+              ^^^
+              <$Quantifier { min: 1, max: +Infinity }>
+                element+$: <//>
+                sigilToken*: <*Keyword '+' />
+              </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/[-]/`', () => {
       expect(print(regex`/[-]/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$CharacterClass { negate: false }>
-              openToken*: <* '[' />
-              elements[]+$: <*Character '-' />
-              closeToken*: <* ']' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <$CharacterClass { negate: false }>
+                openToken*: <* '[' />
+                elements[]+$: <*Character '-' />
+                closeToken*: <* ']' />
+              </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/[--]/`', () => {
       expect(print(regex`/[--]/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$CharacterClass { negate: false }>
-              openToken*: <* '[' />
-              elements[]+$: <*Character '-' />
-              elements[]+$: <*Character '-' />
-              closeToken*: <* ']' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <$CharacterClass { negate: false }>
+                openToken*: <* '[' />
+                elements[]+$: <*Character '-' />
+                elements[]+$: <*Character '-' />
+                closeToken*: <* ']' />
+              </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/[---]/`', () => {
       expect(print(regex`/[---]/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$CharacterClass { negate: false }>
-              openToken*: <* '[' />
-              elements[]+$: <*Character '-' />
-              ^^^
-              <$CharacterClassRange>
-                min+$: <//>
-                sigilToken*: <* '-' />
-                max+$: <*Character '-' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <$CharacterClass { negate: false }>
+                openToken*: <* '[' />
+                elements[]+$: <*Character '-' />
+                ^^^
+                <$CharacterClassRange>
+                  min+$: <//>
+                  sigilToken*: <* '-' />
+                  max+$: <*Character '-' />
+                </>
+                closeToken*: <* ']' />
               </>
-              closeToken*: <* ']' />
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`//i`', () => {
       expect(print(regex`//i`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$: <$Alternative />
-          closeToken*: <* '/' />
-          flags$:
-          <$Flags { global: false, ignoreCase: true, multiline: false, dotAll: false, unicode: false, sticky: false }>
-            tokens[]*: <*Keyword 'i' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$: <$Alternative />
+            closeToken*: <* '/' />
+            flags$:
+            <$Flags { global: false, ignoreCase: true, multiline: false, dotAll: false, unicode: false, sticky: false }>
+              tokens[]*: <*Keyword 'i' />
+            </>
           </>
         </>\n`);
     });
 
     it('`//mi`', () => {
       expect(print(regex`//mi`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$: <$Alternative />
-          closeToken*: <* '/' />
-          flags$:
-          <$Flags { global: false, ignoreCase: true, multiline: true, dotAll: false, unicode: false, sticky: false }>
-            tokens[]*: <*Keyword 'm' />
-            tokens[]*: <*Keyword 'i' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$: <$Alternative />
+            closeToken*: <* '/' />
+            flags$:
+            <$Flags { global: false, ignoreCase: true, multiline: true, dotAll: false, unicode: false, sticky: false }>
+              tokens[]*: <*Keyword 'm' />
+              tokens[]*: <*Keyword 'i' />
+            </>
           </>
         </>\n`);
     });
 
     it('`/\\W/`', () => {
       expect(print(regex`/\W/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$WordCharacterSet { negate: true }>
-              escapeToken*: <* '${'\\\\'}' />
-              value*: <*Keyword 'W' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <$WordCharacterSet { negate: true }>
+                escapeToken*: <* '${'\\\\'}' />
+                value*: <*Keyword 'W' />
+              </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/\\g/`', () => {
       expect(print(regex`/\g/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$Gap>
-              escapeToken*: <* '${'\\\\'}' />
-              value*: <*Keyword 'g' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <$Gap>
+                escapeToken*: <* '${'\\\\'}' />
+                value*: <*Keyword 'g' />
+              </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/\\</`', () => {
       expect(print(regex`/\</`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <*Character>
-              @:
-              <EscapeSequence { cooked: '<' }>
-                escape*: <* '${'\\\\'}' />
-                code*: <*Keyword '<' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
+              elements[]+$:
+              <*Character>
+                @:
+                <EscapeSequence { cooked: '<' }>
+                  escape*: <* '${'\\\\'}' />
+                  code*: <*Keyword '<' />
+                </>
               </>
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>\n`);
     });
 
     it('`/[ \\t]+/`', () => {
       expect(print(regex`/[ \t]+/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$CharacterClass { negate: false }>
-              openToken*: <* '[' />
-              elements[]+$: <*Character ' ' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
               elements[]+$:
-              <*Character>
-                @:
-                <EscapeSequence { cooked: '${'\\t'}' }>
-                  escape*: <* '${'\\\\'}' />
-                  code*: <*Keyword 't' />
+              <$CharacterClass { negate: false }>
+                openToken*: <* '[' />
+                elements[]+$: <*Character ' ' />
+                elements[]+$:
+                <*Character>
+                  @:
+                  <EscapeSequence { cooked: '${'\\t'}' }>
+                    escape*: <* '${'\\\\'}' />
+                    code*: <*Keyword 't' />
+                  </>
                 </>
+                closeToken*: <* ']' />
               </>
-              closeToken*: <* ']' />
+              ^^^
+              <$Quantifier { min: 1, max: +Infinity }>
+                element+$: <//>
+                sigilToken*: <*Keyword '+' />
+              </>
             </>
-            ^^^
-            <$Quantifier { min: 1, max: +Infinity }>
-              element+$: <//>
-              sigilToken*: <*Keyword '+' />
-            </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>
       `);
     });
 
     it('`/[\\u{1}-\\u{10}]/`', () => {
       expect(print(regex`/[\u{1}-\u{10ffff}]/`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$:
-          <$Alternative>
-            elements[]+$:
-            <$CharacterClass { negate: false }>
-              openToken*: <* '[' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$:
+            <$Alternative>
               elements[]+$:
-              <*Character>
-                @:
-                <EscapeSequence { cooked: '${'\\'}u0001' }>
-                  escape*: <* '${'\\\\'}' />
-                  code*:
-                  <EscapeCode>
-                    type*: <*Keyword 'u' />
-                    openToken*: <* '{' />
-                    value: <*UnsignedHexInteger '1' />
-                    closeToken*: <* '}' />
-                  </>
-                </>
-              </>
-              ^^^
-              <$CharacterClassRange>
-                min+$: <//>
-                sigilToken*: <* '-' />
-                max+$:
+              <$CharacterClass { negate: false }>
+                openToken*: <* '[' />
+                elements[]+$:
                 <*Character>
                   @:
-                  <EscapeSequence { cooked: '􏿿' }>
+                  <EscapeSequence { cooked: '${'\\'}u0001' }>
                     escape*: <* '${'\\\\'}' />
                     code*:
                     <EscapeCode>
                       type*: <*Keyword 'u' />
                       openToken*: <* '{' />
-                      value: <*UnsignedHexInteger '10ffff' />
+                      value: <*UnsignedHexInteger '1' />
                       closeToken*: <* '}' />
                     </>
                   </>
                 </>
+                ^^^
+                <$CharacterClassRange>
+                  min+$: <//>
+                  sigilToken*: <* '-' />
+                  max+$:
+                  <*Character>
+                    @:
+                    <EscapeSequence { cooked: '􏿿' }>
+                      escape*: <* '${'\\\\'}' />
+                      code*:
+                      <EscapeCode>
+                        type*: <*Keyword 'u' />
+                        openToken*: <* '{' />
+                        value: <*UnsignedHexInteger '10ffff' />
+                        closeToken*: <* '}' />
+                      </>
+                    </>
+                  </>
+                </>
+                closeToken*: <* ']' />
               </>
-              closeToken*: <* ']' />
             </>
+            closeToken*: <* '/' />
+            flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
           </>
-          closeToken*: <* '/' />
-          flags$: <$Flags { global: false, ignoreCase: false, multiline: false, dotAll: false, unicode: false, sticky: false } />
         </>
       `);
     });
@@ -325,13 +370,16 @@ describe('@bablr/language-en-regex-vm-pattern', () => {
     it('`//<gåp>`', () => {
       const flags = buildRegexTag('Flags')`i`;
       expect(print(regex`//${flags}`)).toEqual(dedent`\
-        <$Pattern>
-          openToken*: <* '/' />
-          alternatives[]$: <$Alternative />
-          closeToken*: <* '/' />
-          flags$:
-          <$Flags { global: false, ignoreCase: true, multiline: false, dotAll: false, unicode: false, sticky: false }>
-            tokens[]*: <*Keyword 'i' />
+        <$_>
+          _:
+          <$Pattern>
+            openToken*: <* '/' />
+            alternatives[]$: <$Alternative />
+            closeToken*: <* '/' />
+            flags$:
+            <$Flags { global: false, ignoreCase: true, multiline: false, dotAll: false, unicode: false, sticky: false }>
+              tokens[]*: <*Keyword 'i' />
+            </>
           </>
         </>\n`);
     });
