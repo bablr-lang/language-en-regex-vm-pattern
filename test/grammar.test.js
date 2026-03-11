0b247b6c0db63fda7b39f8319dc631519a01294d
Conrad Buck <conartist6@gmail.com>
2026-03-11 08:45:54 -0600

    fixesz

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,19 +1,15 @@
+import '@bablr/deep-freeze/register';
 import { buildTag } from 'bablr';
 import { spam } from '@bablr/boot';
 import { dedent } from '@qnighy/dedent';
 import language from '@bablr/language-en-regex-vm-pattern';
-import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { expect } from 'expect';
 import { printPrettyCSTML } from '@bablr/helpers/tree';
 import { buildIdentifier } from '@bablr/helpers/builders';
 
-let enhancers = {};
-
-// enhancers = debugEnhancers;
-
 const buildRegexTag = (type) => {
   const matcher = spam`<$${buildIdentifier(type)} />`;
-  return buildTag(language, matcher, undefined, { enhancers });
+  return buildTag(language, matcher, undefined);
 };
 
 const print = (tree) => {
